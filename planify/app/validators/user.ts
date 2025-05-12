a4922e8eb55ffc96c318d91e82d846d7400da33f
JESSICA6-ZABIBU <jesszabibu15@gmail.com>
2025-05-12 15:42:24 +0200

    modification du validator et le model adapter avec le nom utiliser dans la model et le edge

--- planify/app/validators/user.ts
+++ planify/app/validators/user.ts
@@ -1,1 +1,9 @@
 import vine from '@vinejs/vine'
+
+export const createUserValidator = vine.compile(
+  vine.object({
+    title: vine.string().trim().minLength(6),
+    slug: vine.string().trim(),
+    description: vine.string().trim().escape(),
+  })
+)
