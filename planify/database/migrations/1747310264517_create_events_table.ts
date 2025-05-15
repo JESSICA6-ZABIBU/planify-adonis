4e673da3353329efc9b9c386954261665be26c2d
JESSICA6-ZABIBU <jesszabibu15@gmail.com>
2025-05-15 14:14:25 +0200

    creation des tables

--- planify/database/migrations/1747310264517_create_events_table.ts
+++ planify/database/migrations/1747310264517_create_events_table.ts
@@ -6,6 +6,8 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.integer('users_id').references('id').inTable('users').onDelete('CASCADE').notNullable
+
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
